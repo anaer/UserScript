5d5b56f44f1f8af31c0a7aa496c566be6c43b20b
asdf <asdf@gh.jkl>
2023-09-04 16:05:13 +0800

    添加处理imgur图片

--- Convert-Image-Link-to-Img-Tag.user.js
+++ Convert-Image-Link-to-Img-Tag.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         显示网页中的图片链接
 // @namespace    https://github.com/anaer/UserScript
-// @version      1.2
+// @version      1.3
 // @description  Converts image links to <img> tags on webpages
 // @description:zh-CN 转换页面上的图片链接为img标签进行展示
 // @author       anaer
@@ -30,6 +30,30 @@
 
         // 设置 <img> 标签的 src 属性为图片链接的地址
         img.src = link.href;
+
+        // 添加 CSS 样式，使图片自适应大小
+        img.style.maxWidth = "100%";
+        img.style.height = "auto";
+
+        // 替换图片链接为 <img> 标签
+        link.parentNode.replaceChild(img, link);
+    });
+
+    // 处理imgur图片链接
+    // 测试页面: https://www.v2ex.com/t/970489
+    var imageUrs = document.querySelectorAll('a[href^="https://imgur.com/"]');
+
+    // 遍历每个图片链接
+    imageUrs.forEach(function(link) {
+
+        const url = link.href;
+        const replacedUrl = url.replace(/imgur\.com\/([\w\d]+)/, "i.imgur.com/$1_d.webp?maxwidth=760&fidelity=grand");
+
+        // 创建 <img> 标签元素
+        var img = document.createElement('img');
+
+        // 设置 <img> 标签的 src 属性为图片链接的地址
+        img.src = replacedUrl;
 
         // 添加 CSS 样式，使图片自适应大小
         img.style.maxWidth = "100%";
