06b99c9dc9cb0d8a236122b4295c93e4de94d156
ljksjd <alk@kjsd.csdf>
2023-10-17 16:57:30 +0800

    调整下拉框样式

--- user.js/webpage-translate.user.js
+++ user.js/webpage-translate.user.js
@@ -3,6 +3,7 @@
 // @namespace    https://github.com/anaer/UserScript
 // @match       https://news.ycombinator.com/*
 // @match       https://www.cell.com/*
+// @match       https://zed.dev/*
 // @grant       none
 // @version     1.0.2
 // @author      anaer
@@ -17,9 +18,12 @@
     script.type = 'text/javascript';
     script.src = 'https://res.zvo.cn/translate/translate.js';
     script.onload = script.onreadystatechange = function () {
-        translate.storage.set('to', 'chinese_simplified');
+        translate.storage.set('to', '');
         //设置使用v2.x 版本
         translate.setUseVersion2();
+
+        //设置下拉框显示的语言列表
+        translate.selectLanguageTag.languages = 'chinese_simplified,english'
 
         //SELECT 修改 onchange 事件
         translate.selectLanguageTag.selectOnChange = function (event) {
@@ -31,24 +35,22 @@
         translate.execute();
         document.getElementById('translate').style.position = 'fixed';
         document.getElementById('translate').style.left = '10px';
-        document.getElementById('translate').style.bottom = '10px';
+        document.getElementById('translate').style.top = '10px';
         document.getElementById('translate').style.zIndex = '9999999999999';
 
-
-        setInterval(function () {
-            try {
-                if (document.getElementById('translateSelectLanguage') == null) {
-                    return;
-                }
-                document.getElementById('translateSelectLanguage').style.fontSize = '2rem';
-                document.getElementById('translateSelectLanguage').style.width = '150px';
-            } catch (e) {
-                //select数据是通过接口返回的
-            }
-        }, 1000);
+        // 修改下拉框样式, 默认样式还好 不用修改了
+        // setInterval(function () {
+        //     try {
+        //         if (document.getElementById('translateSelectLanguage') == null) {
+        //             return;
+        //         }
+        //         document.getElementById('translateSelectLanguage').style.fontSize = '2rem';
+        //         document.getElementById('translateSelectLanguage').style.width = '150px';
+        //     } catch (e) {
+        //         //select数据是通过接口返回的
+        //     }
+        // }, 1000);
 
     }
     head.appendChild(script);
-
-
 })();
