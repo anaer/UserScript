220d731dded79ea8ea185270421f8b06b8afc05d
random <a@b.com>
2024-05-10 13:37:18 +0800

    添加账户余额链接

--- user.js/v2ex-flamewar.user.js
+++ user.js/v2ex-flamewar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                v2ex Tab栏添加flamewar
 // @namespace           https://github.com/anaer/UserScript
-// @version             2024.5.7.1043
+// @version             2024.5.10.1335
 // @description         v2ex Tab栏添加flamewar
 // @author              anaer
 // @match               https://*.v2ex.com/*
@@ -15,14 +15,24 @@
 (function () {
   "use strict";
   function addFlamewarNode() {
-    var flamewarNode = document.querySelector(".flamewar");
-    if (flamewarNode == null) {
-      var tabs = document.querySelector("#Tabs");
-      if (tabs != null) {
+    var tabs = document.querySelector("#Tabs");
+    if (tabs != null) {
+      var flamewarNode = document.querySelector(".flamewar");
+      if (flamewarNode == null) {
         var tempNode = document.createElement("a");
         tempNode.href = "https://www.v2ex.com/go/flamewar";
         tempNode.innerHTML = "水深火热";
         tempNode.className = "tab v2p-hover-btn flamewar ";
+        // console.log(tempNode)
+        tabs.appendChild(tempNode);
+      }
+
+      var balanceNode = document.querySelector(".balance");
+      if (balanceNode == null) {
+        var tempNode = document.createElement("a");
+        tempNode.href = "https://v2ex.com/balance";
+        tempNode.innerHTML = "账户余额";
+        tempNode.className = "tab v2p-hover-btn balance";
         // console.log(tempNode)
         tabs.appendChild(tempNode);
       }
