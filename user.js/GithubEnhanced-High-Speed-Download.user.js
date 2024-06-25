ef11023955ca5c8c4eff29e881b06b79518726fd
random <a@b.com>
2024-06-25 09:24:25 +0800

    更新加速源

--- user.js/GithubEnhanced-High-Speed-Download.user.js
+++ user.js/GithubEnhanced-High-Speed-Download.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   Github 增强 - 高速下载
 // @name:zh-TW   Github 增强 - 高速下载
 // @name:en      Github Enhancement - High Speed Download
-// @version      2024.06.24.1403
+// @version      2024.06.25.923
 // @author       X.I.U
 // @description  高速下载 Git Clone/SSH、Release、Raw、Code(ZIP) 等文件、项目列表单文件快捷下载 (☁)
 // @description:zh-CN  高速下载 Git Clone/SSH、Release、Raw、Code(ZIP) 等文件、项目列表单文件快捷下载 (☁)
@@ -71,6 +71,7 @@
     ], clone_ssh_url = [
         ['git@ssh.fastgit.org', '香港', '[中国 香港] - 该公益加速源由 [FastGit] 提供'],
     ], raw_url = [
+        ['https://gh.con.sh/https://raw.githubusercontent.com', '网络', ''],
         ['https://git.988896.xyz/https://raw.githubusercontent.com', '网络', ''],
         ['https://raw.bunnyxyz.eu.org/https://raw.githubusercontent.com', '网络', ''],
         ['https://cf.ghproxy.cc/https://raw.githubusercontent.com', '美国', '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii lau] 提供'],
