11999111e19ac5b3e8a789eb4de24cf4b1402b30
random <a@b.com>
2024-11-19 15:42:18 +0800

    更新nacos脚本

--- user.js/nacos-style.user.js
+++ user.js/nacos-style.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
-// @name         Nacos页面优化
-// @namespace    https://github.com/anaer/UserScript
-// @version 2024.3.18.1416
-// @description Nacos配置详情页配置内容编辑框 高度调整
+// @name         Nacos Style
+// @name:zh-CN   Nacos页面优化
+// @version      24.1119.1536
+// @description  Nacos配置详情页配置内容编辑框 高度调整
 // @author anaer
 // @grant none
 // @noframes
+// @include http://*/nacos/*
 // @include http://*/nacos/#/configdetail?*
 // @include http://*/nacos/#/config*
 // @icon https://nacos.io/favicon.ico
+// @namespace    https://github.com/anaer/UserScript
 // ==/UserScript==
 /*jshint multistr: true */
 
 var styleEl = document.createElement('style');
-styleEl.type = 'text/css';
+// styleEl.type = 'text/css';
 styleEl.innerHTML = "\
 #container{\
 	/* 调整编辑框高度, 适配浏览器*/\
